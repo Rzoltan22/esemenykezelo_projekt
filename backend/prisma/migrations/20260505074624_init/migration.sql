018ad6042755c53f3dd943622677080294a4b12e
Rzoltan22 <raczzoltan38@gmail.com>
2026-05-05 09:47:19 +0200

    feat: adatbazis sema es relaciok letrehozasa Prisma segitsegevel

---
+++ backend/prisma/migrations/20260505074624_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "date" DATETIME NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Registration" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "eventId" INTEGER NOT NULL,
+    CONSTRAINT "Registration_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
